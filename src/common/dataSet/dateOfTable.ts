2f387ebd74bc3d690c60095b2e284928ca18b5fe
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-08-10 01:20:17 +0300

    feat (data ) added data for tableCell

---
+++ src/common/dataSet/dateOfTable.ts
@@ -0,0 +1,9 @@
+export type tableRowsArrayType = {
+    title: string
+}
+export const tableRowsArray: tableRowsArrayType[] = [
+    {title: "Ф.И.О"},
+    {title: "Комиссионный/Стрелочный"},
+    {title: "Станция/Перегон"},
+    {title: "Действия"},
+]
