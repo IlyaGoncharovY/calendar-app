4a363de52d4a4c57c731bd5278994afb4fbf5753
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-08-11 00:24:33 +0300

    fix (tableSlice) add new function for render rows data and add rows

--- src/store/slices/tableSlice.ts
+++ src/store/slices/tableSlice.ts
@@ -1,31 +1,47 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {AppThunk} from "../config/store.ts";
 
 export type RowsType = {
     name: string,
     task: string,
     location: string,
 }
+export interface RowsTypeWithDate extends RowsType {
+    date: string
+}
+
 interface initialStateType {
-    rows: RowsType[]
+    rows: RowsTypeWithDate[]
 }
 
 const initialState: initialStateType = {
-    rows: [
-        {name: "Smirnov", task: "Komis", location: "VDNH"},
-        {name: "Ivanov", task: "Strel", location: "VDNH-BS"},
-        {name: "Sidorov", task: "Strel", location: "VDNH-Aleks"},
-    ]
+    rows: []
 }
 
 const tableSlice = createSlice({
     name: "selectedDate",
     initialState,
     reducers: {
-        getRowsData: (state, action) => {
-
+        getRowsData: (state) => {
+            const existingData = JSON.parse(localStorage.getItem("rowsData") || "[]")
+            state.rows = existingData
+        },
+        addRowsData: (state, action:PayloadAction<RowsTypeWithDate>) => {
+            state.rows.push(action.payload)
         }
     }
 })
-export const {getRowsData} = tableSlice.actions
+export const {addRowsData, getRowsData} = tableSlice.actions
 
+export const addRowsDataTC = (newRow: RowsTypeWithDate): AppThunk =>
+    async (dispatch) => {
+        try {
+            dispatch(addRowsData(newRow))
+            const existingData = JSON.parse(localStorage.getItem("rowsData") || "[]")
+            const newData = [...existingData, newRow]
+            localStorage.setItem("rowsData", JSON.stringify(newData))
+        } catch (e) {
+            console.log({ e })
+        }
+    };
 export default tableSlice.reducer
