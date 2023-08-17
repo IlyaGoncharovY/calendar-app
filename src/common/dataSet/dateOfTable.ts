d802eadf60e4c2f9764b7c1192d666e537acbc41
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-08-17 18:47:08 +0300

    fix (dateOfTable) added new array tasksArray

--- src/common/dataSet/dateOfTable.ts
+++ src/common/dataSet/dateOfTable.ts
@@ -1,9 +1,20 @@
 export type tableRowsArrayType = {
     title: string
 }
+
+export type tasksArrayType = {
+    task: string
+}
 export const tableRowsArray: tableRowsArrayType[] = [
     {title: "Ф.И.О"},
     {title: "Работы"},
     {title: "Станция/Перегон"},
     {title: "Действия"},
+]
+
+export const tasksArray: tasksArrayType[] = [
+    {task: "Комиссионный"},
+    {task: "Стрелочный"},
+    {task: "Волновод"},
+    {task: "Рабочая комиссия"},
 ]
