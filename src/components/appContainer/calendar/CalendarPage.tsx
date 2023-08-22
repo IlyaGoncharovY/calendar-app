ce92b2371ff8a06a04d5c05ec4f2976f3d48dda5
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-08-22 21:38:29 +0300

    fix (CalendarPage) use custom hook and AddModalWindow.tsx

--- src/components/appContainer/calendar/CalendarPage.tsx
+++ src/components/appContainer/calendar/CalendarPage.tsx
@@ -1,31 +1,26 @@
-import {useState} from "react";
 import {Button} from "@mui/material";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import {LocalizationProvider, StaticDatePicker} from "@mui/x-date-pickers";
 
 import {TableComponent} from "./table/TableComponent.tsx";
 import {getDate} from "../../../store/slices/calendarSlice.ts";
-import {ModalWindow} from "../../../common/modalWindow/ModalWindow.tsx";
+import {useModalWindow} from "../../../common/hooks/useModalWindow.ts";
 import {useAppDispatch, useAppSelector} from "../../../store/config/hook.ts";
+import {AddModalWindow} from "../../../common/modalWindow/basic/addModalWindow/AddModalWindow.tsx";
+
 
 export const CalendarPage = () => {
 
     const selectedDate = useAppSelector(state => state.selectedDate.selectedDate)
     const selectedDateObject = selectedDate ? new Date(selectedDate) : null
-    /**
-     * status for the modal window
-     */
-    const [open, setOpen] = useState(false);
+
+    const {open, setOpen, handleOpen} = useModalWindow()
 
     const dispatch = useAppDispatch()
-
-    const handleOpen = () => setOpen(true)
 
     const handleDateChange = (date: Date | null) => {
         dispatch(getDate(date ? date.toISOString() : null))
     };
-
-    // console.log(selectedDateObject)
 
     return (
         <div>
@@ -38,7 +33,7 @@ export const CalendarPage = () => {
             </LocalizationProvider>
             {selectedDateObject && <Button onClick={handleOpen}>Добавить событие</Button>}
             <TableComponent/>
-            <ModalWindow open={open} setOpen={setOpen} selectedDateObject={selectedDateObject}/>
+            <AddModalWindow open={open} setOpen={setOpen} selectedDateObject={selectedDateObject}/>
         </div>
     );
 };
