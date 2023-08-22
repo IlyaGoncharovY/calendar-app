b9a9d7b02d2d17efddafc9f6cbece3796c738c9a
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-08-22 21:35:06 +0300

    feat (basic modal) added basic modal BasicModalWindow.tsx

---
+++ src/common/modalWindow/basic/BasicModalWindow.tsx
@@ -0,0 +1,40 @@
+import {FC, ReactNode} from "react";
+import {Box, Modal} from "@mui/material";
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+interface IBasicModalWindow {
+    open: boolean
+    setOpen: (value: boolean) => void
+    children: ReactNode
+}
+
+export const BasicModalWindow: FC<IBasicModalWindow> = ({open, setOpen, children}) => {
+
+    const handleClose = () => setOpen(false)
+
+    return (
+        <div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    {children}
+                </Box>
+            </Modal>
+        </div>
+    );
+};
