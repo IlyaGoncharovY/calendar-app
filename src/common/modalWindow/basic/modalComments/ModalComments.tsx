bd848efa649459ad9b1daa6682e409ec662dac4d
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-09-22 18:34:35 +0300

    feat (ModalComponents) new modal window for added new comments

---
+++ src/common/modalWindow/basic/modalComments/ModalComments.tsx
@@ -0,0 +1,73 @@
+import {FC, useEffect, useState} from "react";
+import {Button, TextField, Typography} from "@mui/material";
+
+import {BasicModalWindow} from "../BasicModalWindow.tsx";
+import {useAppDispatch} from "../../../../store/config/hook.ts";
+import {addCommentsToRow, saveCommentsToLocalThunk} from "../../../../store/slices/tableReducer/tableSlice.ts";
+
+interface IModalComments {
+    open: boolean
+    setOpen: (value: boolean) => void
+    rowId: string
+}
+
+export const ModalComments: FC<IModalComments> = ({open, setOpen, rowId}) => {
+
+    const [valueInputs, setValueInputs] = useState<{value: string}[]>([{value: ""}])
+
+    const dispatch = useAppDispatch()
+
+    const addInputHandler = () => {
+        setValueInputs(prevState => ([...prevState, {value: ''}]))
+    }
+
+    const deleteInputHandler = () => {
+        if (valueInputs.length > 1) {
+            const newChangeInput = [...valueInputs]
+            newChangeInput.pop()
+            setValueInputs(newChangeInput)
+        }
+    }
+
+    const addCommentHandler = () => {
+        const commentsArray = valueInputs.map((input) => input.value)
+        dispatch(addCommentsToRow({ rowId, comments: commentsArray }))
+        dispatch(saveCommentsToLocalThunk(rowId, commentsArray))
+        setOpen(false)
+    };
+
+    useEffect(() => {
+        if (open) {
+            const storedComments = localStorage.getItem(`comments-${rowId}`)
+            if (storedComments) {
+                const commentsArray = JSON.parse(storedComments)
+                setValueInputs(commentsArray)
+            }
+        }
+    }, [open, rowId])
+
+    return (
+        <div>
+            <BasicModalWindow open={open} setOpen={setOpen}>
+                <Typography id="modal-modal-description1" variant="h6" component="h2">
+                    {valueInputs.map((input, index) => <div key={index}>
+                        <TextField
+                            id={`comments-input-${index}`}
+                            label="Добавить замечания"
+                            variant="standard"
+                            value={input.value}
+                            onChange={(e) => {
+                                const newValueInputs = [...valueInputs]
+                                newValueInputs[index] = { value: e.currentTarget.value }
+                                setValueInputs(newValueInputs)
+                            }}
+                        />
+                    </div>)}
+                </Typography>
+                <Button onClick={addInputHandler} variant={"contained"} style={{margin: "5px"}}>+</Button>
+                <Button onClick={deleteInputHandler} variant={"contained"} disabled={valueInputs.length <= 1}>-</Button>
+                <Button onClick={addCommentHandler}>Сохранить</Button>
+            </BasicModalWindow>
+        </div>
+    );
+};
