90502738b67973b516ef7914b4407c67d8af8399
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-09-22 18:35:44 +0300

    fix (GroupButton) render ModalComments

--- src/common/commonComponents/GroupButton.tsx
+++ src/common/commonComponents/GroupButton.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useState} from "react";
 import dayjs from "dayjs";
 import {Button, ButtonGroup} from "@mui/material";
 
@@ -6,6 +6,7 @@ import {useModalWindow} from "../hooks/useModalWindow.ts";
 import {handleFileDownload} from "../utils/utilFileDownload.ts";
 import {useAppDispatch, useAppSelector} from "../../store/config/hook.ts";
 import {removeRowsDataTC} from "../../store/slices/tableReducer/tableSlice.ts";
+import {ModalComments} from "../modalWindow/basic/modalComments/ModalComments.tsx";
 import {ModalActionWindow} from "../modalWindow/basic/modalAction/ModalActionWindow.tsx";
 
 interface IGroupButton {
@@ -18,6 +19,7 @@ export const GroupButton: FC<IGroupButton> = ({rowId}) => {
     const selectedDate = useAppSelector(state => state.selectedDate.selectedDate)
     const selectedDateObject = selectedDate ? dayjs(selectedDate) : null
 
+    const [commentsModal, setCommentsModal] = useState<boolean>(false)
     const {open, setOpen, handleOpen} = useModalWindow()
 
     const dispatch = useAppDispatch()
@@ -25,11 +27,13 @@ export const GroupButton: FC<IGroupButton> = ({rowId}) => {
     const removeClickHandler = (rowId: string) => {
         dispatch(removeRowsDataTC(rowId))
     }
+    const handleCommentsModal = () => setCommentsModal(true)
 
     return (
         <>
             <ButtonGroup variant="contained" aria-label="outlined primary button group">
                 <Button onClick={() => handleFileDownload(rowId, rows, dispatch)}>Печать</Button>
+                <Button onClick={handleCommentsModal}>Добавить замечания</Button>
                 <Button onClick={handleOpen}>Изменить</Button>
                 <Button onClick={() => removeClickHandler(rowId)}>Удалить</Button>
             </ButtonGroup>
@@ -39,6 +43,7 @@ export const GroupButton: FC<IGroupButton> = ({rowId}) => {
                                selectedDateObject={selectedDateObject}
                                rows={rows}
             />
+            <ModalComments open={commentsModal} setOpen={setCommentsModal} rowId={rowId}/>
         </>
     );
 };
